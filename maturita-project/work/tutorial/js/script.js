e791cd1193ea5b7d2fe48382086fd9282325f329
Jakub Pavlovič <92738343+Slowbat200@users.noreply.github.com>
2023-02-09 12:46:58 +0100

    Delete maturita-project directory

--- maturita-project/work/tutorial/js/script.js
+++
@@ -1,4 +0,0 @@
-const year = document.querySelector("#current-year");
-year.innerHTML = new Date().getFullYear();
-
-
--- maturita-project/work/tutorial/pages/js/title.js
+++
@@ -1,7 +0,0 @@
-var docTitle = document.title;
-window.addEventListener("blur", ()=>{
-    document.title = "Vrať se prosím :(";
-})
-window.addEventListener("focus", ()=>{
-    document.title = docTitle;
-})
